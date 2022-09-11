ff68244c091dc8e416fbb4bdd2d26c9f63285f7e
poludmik <poludmik@fel.cvut.cz>
2022-09-11 12:47:34 +0200

    Finished calendar.

--- src/calendar_generation.py
+++ src/calendar_generation.py
@@ -29,13 +29,16 @@ for year in range(2000, 2100):
     offw = 19
     yearcounter += 1
 
-    d.rectangle([(0 + 10 + rightshift, 26 + downshift), (560 + rightshift, 429 + downshift)], outline=(0, 0, 0), width=3)
+    d.rectangle([(0 + 10 + rightshift, 6 + downshift), (560 + rightshift, 429 + downshift)], outline=(0, 0, 0), width=2)
+    d.line([(0 + 10 + rightshift, 6 + downshift), (560 + rightshift, 6 + downshift)], fill=(0,0,0), width=4)
+    d.line([(0 + 10 + rightshift, 429 + downshift), (560 + rightshift, 429 + downshift)], fill=(0, 0, 0), width=4)
+    d.rectangle([(0 + 10 + rightshift, 6 + downshift), (560 + rightshift, 45 + downshift)], fill=(235, 235, 235), outline=(0, 0, 0), width=1)
     for month in range(1, 13):
-        str1 = plain.formatmonth(year, month)
-        # print(str1)
+        one_month_str = plain.formatmonth(year, month)
+        # print(one_month_str)
         monthcounter += 1
         d.rectangle([(offw + rightshift - 4, offh + downshift - 2), (offw + rightshift + 122, offh + downshift + 117)], outline=(0,0,0), width=1)
-        d.text((offw + rightshift, offh + downshift), str1, (0, 0, 0))
+        d.text((offw + rightshift, offh + downshift), one_month_str, (0, 0, 0))
         offw += 138
         if monthcounter == 4:
             monthcounter = 0
@@ -52,7 +55,7 @@ font = ImageFont.truetype("../fonts/joystix_monospace.ttf", 50)
 
 txt = Image.new('RGBA', img.size, (255,255,255,0))
 new_layer = ImageDraw.Draw(txt)
-new_layer.text((1240, 890), ascii.AsciiArts.skull, font=font, fill=(0, 0, 0, 100))
+new_layer.text((1240, 880), ascii.AsciiArts.skull, font=font, fill=(0, 0, 0, 100))
 combined = Image.alpha_composite(img, txt)
 
 combined.save('../calendar.png')
